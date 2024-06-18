4dd1ca88c0ff2a50f891d47da9ce3812ba0bc05c
khaymanii <kennethakpo61@gmail.com>
2024-06-18 21:00:28 +0100

    fixed mobile responsiveness

--- components/BrowseCategories.tsx
+++ components/BrowseCategories.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import Button from "./Button";
 
+const categories = [
+  { name: "Holiday Rentals", padding: "sm:px-16 px-8" },
+  { name: "Residential Spaces", padding: "sm:px-10 px-6" },
+  { name: "Event Spaces", padding: "sm:px-16 px-8" },
+  { name: "Commercial Properties", padding: "sm:px-8 px-4" },
+  { name: "More", padding: "sm:px-24 px-12" },
+];
+
 function BrowseCategories() {
   function handleClick() {
     console.log("Button clicked!");
@@ -11,37 +19,16 @@ function BrowseCategories() {
       <h1 className="sm:text-4xl text-2xl font-bold mb-4 text-center">
         Browse From Top Categories
       </h1>
-      <div className="flex flex-col sm:flex-row items-center justify-around mt-4 sm:pt-4">
-        <Button
-          className="w-80 sm:w-auto px-16 mb-4 sm:mb-0 sm:mr-4 py-4"
-          onClick={handleClick}
-        >
-          Holiday Rentals
-        </Button>
-        <Button
-          className="w-80 sm:w-auto px-10 mb-4 sm:mb-0 sm:mr-4 py-4"
-          onClick={handleClick}
-        >
-          Residential spaces
-        </Button>
-        <Button
-          className="w-80 sm:w-auto px-16 mb-4 sm:mb-0 sm:mr-4 py-4"
-          onClick={handleClick}
-        >
-          Event Spaces
-        </Button>{" "}
-        <Button
-          className="w-80 sm:w-auto px-8 mb-4 sm:mb-0 sm:mr-4 py-4"
-          onClick={handleClick}
-        >
-          Commercial Properties
-        </Button>
-        <Button
-          className="w-80 sm:w-auto px-24 mb-4 sm:mb-0 sm:mr-4 py-4"
-          onClick={handleClick}
-        >
-          More
-        </Button>
+      <div className="flex flex-wrap items-center justify-center sm:justify-around mt-4 sm:pt-4">
+        {categories.map((category) => (
+          <Button
+            key={category.name}
+            className={`w-full sm:w-auto ${category.padding} mb-4 sm:mb-0 sm:mr-4 py-2 sm:py-4`}
+            onClick={handleClick}
+          >
+            {category.name}
+          </Button>
+        ))}
       </div>
     </div>
   );
