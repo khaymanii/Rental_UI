b7f47239039d32cb593324aae4cd2370bcfcbace
khaymanii <kennethakpo61@gmail.com>
2024-06-28 23:21:31 +0100

    created new component

---
+++ components/ImportantArticle.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ImportantArticle() {
+  return (
+    <div className="max-w-7xl mx-auto my-10 px-4 sm:px-8">
+      <h1 className="text-2xl sm:text-5xl font-bold mb-8 text-center">
+        Important Articles
+      </h1>
+    </div>
+  );
+}
+
+export default ImportantArticle;
