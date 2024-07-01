23966d7fb2c4b4ee0bf31e08ffcd0e64fb35fc91
khaymanii <kennethakpo61@gmail.com>
2024-07-01 15:03:49 +0100

    fixed mobile responsiveness

--- components/ImportantArticle.tsx
+++ components/ImportantArticle.tsx
@@ -11,8 +11,8 @@ function ImportantArticle() {
       <h1 className="text-2xl sm:text-5xl font-bold mb-8 text-center">
         Important Articles
       </h1>
-      <div className="flex items-start justify-between">
-        <div className="w-1/2 mr-6">
+      <div className="flex flex-col lg:flex-row items-start justify-between">
+        <div className="w-full lg:w-1/2 lg:mr-6 mb-6 lg:mb-0">
           <Image
             src={Article1}
             alt="article1 image"
@@ -20,81 +20,90 @@ function ImportantArticle() {
             height={700}
             objectFit="cover"
             layout="responsive"
-            className="pb-4 rounded-lg"
+            className="pb-4"
           />
           <p className="text-red-500 font-semibold pb-2">February 4, 2023</p>
-          <h1 className="font-bold text-2xl pb-4">
+          <h1 className="font-bold text-xl pb-4">
             Top Tip for Finding the Perfect Rental
           </h1>
-          <p className="text-gray-500 font-light text-lg">
+          <p className="text-gray-500 font-light text-lg tracking-wider">
             Discover expert advice on how to find the ideal rental property that
             meets your needs and budget.
           </p>
         </div>
-        <div className="w-1/2">
-          <div className="flex mb-6">
-            {" "}
-            <Image
-              src={Article2}
-              alt="article2 image"
-              width={200}
-              height={100}
-              objectFit="cover"
-              className="rounded-lg mr-4"
-            />
-            <div className="">
-              <p className="text-red-500 font-semibold pb-2">May 7, 2023</p>
-              <h1 className="font-bold text-xl pb-2">
-                {" "}
-                Bike Rental Hacks for a Smooth Journey
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col sm:flex-row mb-5">
+            <div className="w-full sm:w-[200px] sm:mr-4 mb-4 sm:mb-0">
+              <Image
+                src={Article2}
+                alt="article2 image"
+                width={300}
+                height={200}
+                objectFit="cover"
+                layout="responsive"
+                className="w-full h-auto"
+              />
+            </div>
+            <div className="w-full sm:flex-1">
+              <p className="text-red-500 font-semibold pb-2">
+                February 4, 2023
+              </p>
+              <h1 className="font-bold text-xl pb-4">
+                Top Tip for Finding the Perfect Rental
               </h1>
-              <p className="text-gray-500 font-light text-sm tracking-wider">
-                Learn insider tips for getting the best deals and making the
-                most of your rental bike experience.
+              <p className="text-gray-500 font-light text-base tracking-wider">
+                Discover expert advice on how to find the ideal rental property
+                that meets your needs and budget.
               </p>
             </div>
           </div>
-          <div className="flex mb-6">
-            {" "}
-            <Image
-              src={Article3}
-              alt="article2 image"
-              width={200}
-              height={100}
-              objectFit="cover"
-              className="rounded-lg mr-4"
-            />
-            <div className="">
-              <p className="text-red-500 font-semibold pb-2">April 25, 2023</p>
-              <h1 className="font-bold text-xl pb-2">
-                {" "}
-                Managing Your Rental Items
+          <div className="flex flex-col sm:flex-row mb-5">
+            <div className="w-full sm:w-[200px] sm:mr-4 mb-4 sm:mb-0">
+              <Image
+                src={Article3}
+                alt="article3 image"
+                width={300}
+                height={200}
+                objectFit="cover"
+                layout="responsive"
+                className="w-full h-auto"
+              />
+            </div>
+            <div className="w-full sm:flex-1">
+              <p className="text-red-500 font-semibold pb-2">
+                February 4, 2023
+              </p>
+              <h1 className="font-bold text-xl pb-4">
+                Top Tip for Finding the Perfect Rental
               </h1>
-              <p className="text-gray-500 font-light text-sm tracking-wider">
-                Get valuable insights on how to effectively manage and maintain
-                remeditems.
+              <p className="text-gray-500 font-light text-base tracking-wider">
+                Discover expert advice on how to find the ideal rental property
+                that meets your needs and budget.
               </p>
             </div>
           </div>
-          <div className="flex mb-6">
-            {" "}
-            <Image
-              src={Article4}
-              alt="article4 image"
-              width={200}
-              height={100}
-              objectFit="cover"
-              className="rounded-lg mr-4"
-            />
-            <div className="">
-              <p className="text-red-500 font-semibold pb-2">January 2, 2023</p>
-              <h1 className="font-bold text-xl pb-2">
-                {" "}
-                Navigating the Rental Market
+          <div className="flex flex-col sm:flex-row">
+            <div className="w-full sm:w-[200px] sm:mr-4 mb-4 sm:mb-0">
+              <Image
+                src={Article4}
+                alt="article4 image"
+                width={300}
+                height={200}
+                objectFit="cover"
+                layout="responsive"
+                className="w-full h-auto"
+              />
+            </div>
+            <div className="w-full sm:flex-1">
+              <p className="text-red-500 font-semibold pb-2">
+                February 4, 2023
+              </p>
+              <h1 className="font-bold text-xl pb-4">
+                Top Tip for Finding the Perfect Rental
               </h1>
-              <p className="text-gray-500 font-light text-sm tracking-wider">
-                Explore key consideration and useful tips for individuals new to
-                the rental market.
+              <p className="text-gray-500 font-light text-base tracking-wider">
+                Discover expert advice on how to find the ideal rental property
+                that meets your needs and budget.
               </p>
             </div>
           </div>
